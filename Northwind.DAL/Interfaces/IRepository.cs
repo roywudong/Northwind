cc0183931586e7fbba8018a32b88d75f56fb3f2a
roywudong <roywudong@gmail.com>
2019-03-08 17:05:26 +0800

    Complete Data Access Layer by using Repository Pattern

---
+++ Northwind.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Northwind.DAL.Interfaces
+{
+  internal interface IRepository<TEntity> where TEntity : class
+  {
+    IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>,
+            IOrderedQueryable<TEntity>> orderBy = null);
+
+    TEntity GetByID(object id);
+
+    void Update(TEntity entity);
+
+    void Delete(object id);
+
+    void Delete(TEntity entity);
+
+    void Insert(TEntity entity);
+  }
+}
